b063d73a7d74ed2370a8b93b7dd1d348cb30454a
thebasedtaka <thebasedtaka@CodeVM.myguest.virtualbox.org>
2023-05-23 23:59:09 -0400

    work pls

---
+++ javaMovie/src/main/java/dev/taka/movies/MoviesApplication.java
@@ -0,0 +1,25 @@
+package dev.taka.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiIndex(){
+		return "Sup Bitches";
+	}
+
+	@GetMapping("/sfw")
+	public String apiIndex2(){
+		return "Sup friends";
+	}
+}
